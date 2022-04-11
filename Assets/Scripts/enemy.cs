06ea409eab4cfd24be054717ab682432931f447b
tulay <tmagar448@gmail.com>
2022-04-10 20:31:23 -0400

    need to investigate a bug

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
-    // public GameObject projectile;
-
-    public float movementDirection;
+    public GameObject projectile;
     private float timerShoot;
     private float xRange = 8.4f;
 
-    // Start is called before the first frame update
+    float timer = 0;
+    float timeToMove = 0.5f;
+    int numofMovements = 0;
+    float speed = 3.2f;
+
     void Start()
     {
 
@@ -23,16 +25,40 @@ public class enemy : MonoBehaviour
 
         CheckOutOfBounds();
 
-        if (movementDirection == 1)
-        { // right
-            transform.position = new Vector2(transform.position.x + 0.2f, transform.position.y);
+        // if (movementDirection == 1)
+        // { // right
+        //     transform.position = new Vector2(transform.position.x + 0.2f, transform.position.y);
+        //     // timerShoot = 0;
+        // }
+        // if (movementDirection == -1)
+        // { // left
+        //     transform.position = new Vector2(transform.position.x - 0.2f, transform.position.y);
+        //     // timerShoot = 0;
+        // }
+
+        if (numofMovements == 3)
+        {
+            transform.Translate(new Vector2(speed, 0));
+            numofMovements = -1;
+            speed = -speed;
+            timer = 0;
+            Instantiate(projectile, transform.position, projectile.transform.rotation);
             timerShoot = 0;
         }
-        else if (movementDirection == -1)
-        { // left
-            transform.position = new Vector2(transform.position.x - 0.2f, transform.position.y);
+
+        timer += Time.deltaTime;
+        if (timer > timeToMove && numofMovements < 3)
+        {
+            transform.Translate(new Vector2(speed, 0));
+
+            timer = 0;
+            numofMovements++;
+            Instantiate(projectile, transform.position, projectile.transform.rotation);
             timerShoot = 0;
         }
+
+
+
     }
 
     private void CheckOutOfBounds()
@@ -47,4 +73,6 @@ public class enemy : MonoBehaviour
             transform.position = new Vector2(xRange, transform.position.y);
         }
     }
+
+    // https://youtu.be/2mNXfrh0UYo
 }
