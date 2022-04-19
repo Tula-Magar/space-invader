027513efaf15aee121ed66487c0d7186e1c9a7ca
tulay <tmagar448@gmail.com>
2022-04-19 10:32:10 -0400

    live killing score

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private GameManager gameManager;
+    public GameObject projectile;
+
+    private float movementDirection;
+    private float timerMove;
+    private float timerShoot;
+    private float xRange;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager.AddEnemy(gameObject);   // modified Observer pattern
+
+        movementDirection = -1;
+        xRange = 9.0f;
+    }
+
+    void Update()
+    {
+        timerMove += Time.deltaTime;
+        timerShoot += Time.deltaTime;
+
+        CheckOutOfBounds();
+        
+        if (timerShoot > 3.0f) {
+            Instantiate(projectile, transform.position, projectile.transform.rotation);
+            timerShoot = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (timerMove > 1.0f) {
+            if (movementDirection == 1) { // right
+                transform.position = new Vector2(transform.position.x + 1.0f, transform.position.y);
+            } else if (movementDirection == -1) { // left
+                transform.position = new Vector2(transform.position.x - 1.0f, transform.position.y);
+            }
+            timerMove = 0;
+        }
+    }
+
+    private void CheckOutOfBounds()
+    {
+        if (Mathf.Abs(transform.position.x) >= xRange) { gameManager.EnemiesChangeDirection(); }
+    }
+
+    public void ChangeDirection()
+    {
+        if (movementDirection == 1) {
+            transform.position = new Vector2(transform.position.x - 1, transform.position.y - 1);
+            movementDirection = -1;
+        }
+        else {
+            transform.position = new Vector2(transform.position.x + 1, transform.position.y - 1);
+            movementDirection = 1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player_Projectile")) {
+            gameManager.RemoveEnemy(gameObject);
+            gameManager.ScoreNum = 1;
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private List<GameObject> enemies = new List<GameObject>();
+
+    private int Score;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // modified Observer pattern
+    public void AddEnemy(GameObject enemy)
+    {
+        enemies.Add(enemy);
+    }
+
+    public void RemoveEnemy(GameObject enemy)
+    {
+        enemies.Remove(enemy);
+        // Score = Score + 1;
+        // Debug.Log(Score);
+
+    }
+
+    public void EnemiesChangeDirection()
+    {
+        foreach(GameObject enemy in enemies) {
+            enemy.GetComponent<EnemyMovement>().ChangeDirection();
+        }
+    }
+
+    public int ScoreNum {
+        get { return Score;}
+        set {this.Score = this.Score + value;}
+    }
+}
