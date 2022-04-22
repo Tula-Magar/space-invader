8cb0c7cc73558f00fa4260fe0a8cc406a3630372
tulay <tmagar448@gmail.com>
2022-04-22 15:32:51 -0400

    Color change on mouse over and exit from the menu button

--- Scripts/ClickToPlayGame.cs
+++ Scripts/ClickToPlayGame.cs
@@ -2,21 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class ClickToPlayGame : MonoBehaviour
 {
+
     void Start()
     {
-
+        
     }
 
     void Update()
     {
-        
+
     }
 
     void OnMouseDown()
     {
         SceneManager.LoadScene("PlayGame");
     }
+
+    void OnMouseOver()
+    {
+       GetComponent<SpriteRenderer>().color = new Color (
+           255, 
+           0, 
+           187, 
+           255 
+        );
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().color = new Color (
+
+                248,
+                248,
+                248,  
+                255
+        );
+    }
+
+
 }
